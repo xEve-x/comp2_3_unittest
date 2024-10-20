d01463a448ac43db92c737345fdc7531204074bb
imagire <imagire@gmail.com>
2024-10-20 22:24:35 +0900

    add files

---
+++ src/StaticLib/StaticLib.c
@@ -0,0 +1,25 @@
+﻿// StaticLib.c : スタティック ライブラリ用の関数を定義します。
+//
+
+#include "framework.h"// プロジェクトで共有の項目を載せる
+
+#include "../include/my_lib.h"
+
+// num個の整数の和を得る
+int sum(int num, int* array)
+{
+	int sum = 0;
+
+	for (int i = 0; i < num; i++)
+	{
+		sum += array[i];
+	}
+
+	return sum;
+}
+
+// num個の整数の平均を得る
+int average(int num, int* array)
+{
+	return 0x7fffffff;// ■未実装
+}
